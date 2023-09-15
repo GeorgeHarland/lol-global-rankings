0d5526dc8a2fb4496eb0a2157f42e241d6e9ae6f
GeorgeH <georgeharland12@gmail.com>
2023-09-15 22:01:57 +0100

    team player endpoints

--- backend/app.py
+++ backend/app.py
@@ -26,9 +26,64 @@ mock_rankings = [
         }
     ]
 
+mock_teams = [
+        {
+            "team_id": "100205573495116443",
+            "team_code": "GEN",
+            "team_name": "Gen.G",
+        },
+        {
+            "team_id": "98767991877340524",
+            "team_code": "C9",
+            "team_name": "Cloud9",
+        },
+        {
+            "team_id": "99566404853058754",
+            "team_code": "WBG",
+            "team_name": "WeiboGaming FAW AUDI",
+        }
+    ]
+
+mock_players = [
+        {
+            "player_id": "123123",
+            "player_name": "Faker",
+            "player_team": "T1",
+            "past_teams": "none"
+        },
+        {
+            "player_id": "321321",
+            "player_name": "Jensen",
+            "player_team": "C9",
+            "past_teams": "none"
+        },
+    ]
+
 @app.route('/', methods=['GET'])
 def hello():
     return {'message': 'Hello, world!'}
+
+@app.route('/teams', methods=['GET'])
+@app.route('/teams/{team_name}', methods=['GET'])
+def get_team(team_name = ''):
+    # if no team name, provide list of teams
+    if not team_name:
+        return {'teams': mock_teams}
+    for team in mock_teams:
+        if team['team_name'] == team_name:
+            return team
+    return {"error": "Team not found"}
+
+@app.route('/players', methods=['GET'])
+@app.route('/players/{player_name}', methods=['GET'])
+def get_player(player_name = ''):
+    # if no player name, provide list of players? maybe, less important than the teams one
+    if not player_name:
+        return {'players': mock_players}
+    for player in mock_players:
+        if player['player_name'] == player_name:
+            return player
+    return {"error": "Player not found"}
 
 @app.route('/random', methods=['GET'])
 def generate_random_rankings():
