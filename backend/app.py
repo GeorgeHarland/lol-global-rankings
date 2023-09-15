6d7b72efe3d8512c1bb26b48d646606020df47ec
GeorgeH <georgeharland12@gmail.com>
2023-09-15 19:37:10 +0100

    mock endpoints

--- backend/app.py
+++ backend/app.py
@@ -2,32 +2,66 @@ from chalice import Chalice
 import json
 
 from chalicelib.randomly_rank_all_teams import random_ranking
-from chalicelib.win_loss_rank_all_teams import compute_win_loss_ratios
 
 app = Chalice(app_name='blue-buff-lambdas')
 
-@app.route('/')
+mock_rankings = [
+        {
+            "team_id": "100205573495116443",
+            "team_code": "GEN",
+            "team_name": "Gen.G",
+            "rank": 1
+        },
+        {
+            "team_id": "98767991877340524",
+            "team_code": "C9",
+            "team_name": "Cloud9",
+            "rank": 1
+        },
+        {
+            "team_id": "99566404853058754",
+            "team_code": "WBG",
+            "team_name": "WeiboGaming FAW AUDI",
+            "rank": 3
+        }
+    ]
+
+@app.route('/', methods=['GET'])
 def hello():
     return {'message': 'Hello, world!'}
 
-@app.route('/random')
+@app.route('/random', methods=['GET'])
 def generate_random_rankings():
-    filename = 'chalicelib/esports-data/teams.json'
+    filename = 'chalicelib/derived-data/team_winrates.json'
     top_10_rankings = random_ranking(filename)
     return {'top_10_rankings': top_10_rankings}
 
-# @app.route('/winlossgames')
-# def generate_winlossgames_rankings():
-#     top_10_rankings = compute_win_loss_ratios('chalicelib/esports-data/teams.json', 'chalicelib/esports-data/tournaments.json')
-#     return {'top_10_rankings': top_10_rankings}
-
-@app.route('/winlossgames')
+@app.route('/winlossgames', methods=['GET'])
 def generate_winlossgames_rankings():
     with open('chalicelib/derived-data/team_winrates.json', 'r') as infile:
         team_stats = json.load(infile)
-    
     top_10_rankings = team_stats[:10]
     return {'top_10_rankings': top_10_rankings}
+
+@app.route('/tournament_rankings/{tournament_id}', methods=['GET'])
+def get_tournament_rankings(tournament_id):
+    # params: tournament_id (string, path, required*), stage (string, query)
+    # rankings should be historical to the tournament
+    # return {"message": f"Tournament rankings for ID {tournament_id}"}
+    return {"rankings": mock_rankings}
+
+@app.route('/global_rankings', methods=['GET'])
+def get_global_rankings():
+    # params: number_of_teams (integer, query)
+    # return {"message": "Global rankings"}
+    return {"rankings": mock_rankings}
+
+@app.route('/team_rankings', methods=['GET'])
+def get_team_rankings():
+    # params: team_ids (array[string], query, required*)
+    # team_ids = app.current_request.query_params.getlist('team_ids')
+    # return {"message": f"Team rankings for IDs {team_ids}"}
+    return {"rankings": mock_rankings}
 
 if __name__ == '__main__':
     app.debug = True
--- backend/chalicelib/randomly_rank_all_teams.py
+++ backend/chalicelib/randomly_rank_all_teams.py
@@ -1,25 +1,24 @@
-# prints random rankings for all teams in teams.json
-
 import json
 import random
 
 def random_ranking(filename):
     with open(filename, 'r') as f:
-        teams = json.load(f)
-    
-    # Shuffle the teams randomly
+        data = json.load(f)
+
+    # Extract only team details for shuffling.
+    teams = [team_data[1] for team_data in data]
+
     random.shuffle(teams)
-    
-    # Extract the top 10
+
     top_10 = teams[:10]
-    
-    # Prepare the top 10 rankings as a list of dictionaries
-    top_10_rankings = [{'name': team['name'], 'acronym': team['acronym']} for team in top_10]
+
+    # Adapting to the new keys 'teamname' and assuming there's no 'acronym' in the new data format.
+    top_10_rankings = [{'name': team['teamname']} for team in top_10]
 
     return top_10_rankings
 
 if __name__ == "__main__":
-    rankings = random_ranking('esports-data/teams.json')
+    rankings = random_ranking('derived-data/team_winrates.json')
     print("Top 10 Global Rankings:")
     for i, team in enumerate(rankings, 1):
-        print(f"{i}. {team['name']} ({team['acronym']})")
+        print(f"{i}. {team['name']}")
