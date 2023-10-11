bca40121203f066cbcf119dfde863cd66e53e108
GeorgeH <georgeharland12@gmail.com>
2023-10-11 15:22:23 +0100

    navbar 2

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -2,24 +2,24 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="grid grid-flow-col gap-4 auto-cols-max justify-center items-center border-b border-gray-400 mb-4 pb-4">
+    <nav className="grid grid-flow-row gap-6 justify-center items-center border-b border-gray-400 mb-4 pb-4 md:grid-flow-col">
       <Link href="/">
-        <p className="transition-transform transform-gpu hover:scale-105 hover:opacity-80 text-2xl font-semibold font-sans tracking-wide hover:text-gray-400">
+        <p className="transition-transform transform-gpu hover:scale-105 hover:opacity-80 text-2xl font-semibold font-sans tracking-wide hover:text-gray-400 divider">
           Global Leaderboard
         </p>
       </Link>
       <Link href="/worlds">
-        <p className="transition-transform transform-gpu hover:scale-105 hover:opacity-80 text-2xl font-semibold font-sans tracking-wide hover:text-gray-400">
+        <p className="transition-transform transform-gpu hover:scale-105 hover:opacity-80 text-2xl font-semibold font-sans tracking-wide hover:text-gray-400 divider">
           Worlds 2023
         </p>
       </Link>
       <Link href="/teams">
-        <p className="transition-transform transform-gpu hover:scale-105 hover:opacity-80 text-2xl font-semibold font-sans tracking-wide hover:text-gray-400">
+        <p className="transition-transform transform-gpu hover:scale-105 hover:opacity-80 text-2xl font-semibold font-sans tracking-wide hover:text-gray-400 divider">
           Teams
         </p>
       </Link>
       <Link href="/compare">
-        <p className="transition-transform transform-gpu hover:scale-105 hover:opacity-80 text-2xl font-semibold font-sans tracking-wide hover:text-gray-400">
+        <p className="transition-transform transform-gpu hover:scale-105 hover:opacity-80 text-2xl font-semibold font-sans tracking-wide hover:text-gray-400 divider">
           Compare Teams
         </p>
       </Link>
